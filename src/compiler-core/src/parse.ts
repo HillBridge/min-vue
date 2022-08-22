e3a8e951844173f5ff072243766346eeaf792d70
qiaoguosheng <294238921@qq.com>
2022-08-22 11:15:49 +0800

    解析tag，删除处理完成的代码

--- src/compiler-core/src/parse.ts
+++ src/compiler-core/src/parse.ts
@@ -1,4 +1,8 @@
 import { NodeTypes } from "./ast"
+const enum TagType {
+  Start,
+  End
+}
 
 export function baseParse(content:string) {
   const context = createParserContext(content)
@@ -18,7 +22,7 @@ function parseChildren(context) {
     node = parseInterPolation(context)
   }else if(context.source.startsWith("<")){
     if(/[a-z]/i.test(context.source)){
-      node = parseElement(context.source)
+      node = parseElement(context)
     }
   }
   nodes.push(node)
@@ -26,9 +30,26 @@ function parseChildren(context) {
 }
 
 function parseElement(content:any) {
+  const element = parseTag(content, TagType.Start)
+
+  parseTag(content, TagType.End)
+
+  console.log("----",content.source)
+
+  return element
+}
+
+function parseTag(content:any, type: TagType) {
+  //1. 解析tag
+  const match: any = /^<\/?([a-z]*)/i.exec(content.source)
+  const tag = match[1]
+  //2. 删除处理完成的代码
+  advanceBy(content, match[0].length)
+  advanceBy(content, 1)
+  if(type === TagType.End) return
   return {
     type: NodeTypes.ELEMENT,
-    tag: "div"
+    tag
   }
 }
 
